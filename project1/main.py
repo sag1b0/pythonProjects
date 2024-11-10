fa9b682e9b67388108d8684d0b652ccfbd1dfeb6
sag1b0 <sa6xws@gmail.com>
2024-11-10 17:35:32 +0530

    completing project 1

---
+++ project1/main.py
@@ -0,0 +1,19 @@
+# Python Calculator
+
+operator = input ("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(result)
+elif operator == "-":
+    result = num1 - num2
+    print(result)
+elif operator == "*":
+    result = num1 * num2
+    print(result)
+elif operator == "/":
+    result = num1 / num2
+    print(result)
+
